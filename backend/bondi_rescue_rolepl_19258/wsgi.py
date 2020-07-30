5bb634631be051e43bf26b151eaa6cea698db9a0
Crowdbotics <team@crowdbotics.com>
2020-07-30 05:27:55 +0000

    Update models

--- backend/bondi_rescue_rolepl_19258/wsgi.py
+++ backend/bondi_rescue_rolepl_19258/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bondi_rescue_rolepl_19258.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bondi_rescue_rolepl_19258.settings")
 
 application = get_wsgi_application()
